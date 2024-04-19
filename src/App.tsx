7917b9f2241bac4247e9b01d0e12df09c449d44d
signor1 <emmanuelomemgboji@gmail.com>
2024-04-19 18:28:31 +0100

    feat: project setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button";
+
+
+
+const App = () => {
+
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Installed Vite + React + Typescript + Tailwind CSS 3 + React-Router-Dom + Web3Modal + Shadcn
+      </h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App
