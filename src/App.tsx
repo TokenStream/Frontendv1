e53a855f0ce6a56bcc1c6e30de3b2b441d66ae90
signor1 <emmanuelomemgboji@gmail.com>
2024-04-21 14:40:41 +0100

    feat: modified some files

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import { Button } from "./components/ui/button";
 
 
 
+
 const App = () => {
 
   return (
-    <div className="min-h-screen flex justify-center items-center">
+    <div className="min-h-screen flex flex-col justify-center items-center">
       <h1 className="text-3xl font-bold text-blue-600">
         Installed Vite + React + Typescript + Tailwind CSS 3 + React-Router-Dom + Web3Modal + Shadcn
       </h1>
-      <Button>Click me</Button>
+      <Button variant={"outline"}>Hello</Button>
     </div>
   );
 }
