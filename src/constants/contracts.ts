b6497a40d382c3d39c0f0ec938ffdef16b15ca43
signor1 <emmanuelomemgboji@gmail.com>
2024-05-01 17:41:26 +0100

    feat: chore: working on simulating approve & transferFrom inone function call

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -1,5 +1,6 @@
 import { ethers } from "ethers";
 import ENSContractABI from "./ABI/ENSContractABI.json";
+import ModaContractABI from "./ABI/ModaContractABI.json";
 
 export const getENSContract = (
   providerOrSigner: ethers.Provider | ethers.Signer
@@ -7,6 +8,16 @@ export const getENSContract = (
   return new ethers.Contract(
     import.meta.env.VITE_ENS_CONTRACT_ADDRESS,
     ENSContractABI,
+    providerOrSigner
+  );
+};
+
+export const getModalContract = (
+  providerOrSigner: ethers.Provider | ethers.Signer
+) => {
+  return new ethers.Contract(
+    import.meta.env.VITE_MODA_CONTRACT_ADDRESS,
+    ModaContractABI,
     providerOrSigner
   );
 };
