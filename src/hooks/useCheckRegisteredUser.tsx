edaa8b564cb8c9ceb424e536443744a46fcbf73d
signor1 <emmanuelomemgboji@gmail.com>
2024-05-01 04:02:31 +0100

    feat: modified env files

--- src/hooks/useCheckRegisteredUser.tsx
+++ src/hooks/useCheckRegisteredUser.tsx
@@ -1,10 +1,29 @@
 import { useEffect, useState } from "react";
 import { readOnlyProvider } from "@/constants/provider";
+import { getENSContract } from "@/constants/contracts";
 
-export const useCheckRegisteredUser = () => {
+export const useCheckRegisteredUser = (address: any) => {
     const [user, setUser] = useState({});
 
     useEffect(() => {
+        const contract = getENSContract(readOnlyProvider);
 
-    })
+        contract
+            .getUserFromAddress(address)
+            .then((res) => {
+                const converted = {
+                    name: res[0],
+                    avatar: res[1],
+                    address: res[2],
+                };
+
+                setUser(converted);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+
+    }, [address])
+
+    return user;
 }
